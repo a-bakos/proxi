fd5fc559e4b5bb155d31915189a9802c133eddf4
a-bakos <bakosattila@fastem.com>
2017-09-03 22:41:13 +0100

    add and improve basic search features, add last commands list display, etc

--- script.js
+++ script.js
@@ -39,12 +39,26 @@
   }, 3000);
 })();
 
-// Last command display:
-var lastCommand = document.querySelector(".last-command");
-var lastCommandMessage = document.createElement("p");
-lastCommand.appendChild(lastCommandMessage);
 
-// Function for providing voice feedback:
+/**
+ * Last commands section
+ */
+var lastCommandCounter = 0;
+var lastCommand = document.querySelector(".last-command-list");
+
+// Building and appending elements as command items to the last commands list:
+function appendLastCommand(commandMessage) {
+  lastCommandCounter++;
+  var lastCommandMessage = document.createElement("p");
+  lastCommandMessage.classList.add("last-command-item", "command-" + lastCommandCounter);
+  lastCommandMessage.innerHTML = commandMessage;
+  lastCommand.insertBefore(lastCommandMessage, lastCommand.firstChild);
+}
+
+
+/**
+ * Function for providing voice feedback
+ */
 function voiceFeedback(feedbackString, pitch = 1, rate = 1.25) {
   var commandFeedback = new SpeechSynthesisUtterance();
   commandFeedback.text = feedbackString;
@@ -54,35 +68,91 @@ function voiceFeedback(feedbackString, pitch = 1, rate = 1.25) {
   speechSynthesis.speak(commandFeedback);
 }
 
+
+/**
+ * Google search links
+ */
+var googleURL = "https://www.google.co.uk/";
+// Regular search:
+var googleSearchFor = "https://www.google.co.uk/search?source=hp&q=";
+// Image search:
+var imageSearchStart = "https://www.google.co.uk/search?q=";
+var imageSearchEnd = "&source=lnms&tbm=isch";
+var googleImageSearchFor;
+
+/**
+ * Annyang commands
+ */
 if (annyang) {
+  var url;
 
   var myWindow; // For new windows
 
   // Define commands. First the text, and then the function it should call
   var commands = {
     'open search': function() {
-      var url = "https://www.google.co.uk/";
-      myWindow = window.open(url, "myWindow");
-      lastCommandMessage.innerHTML = "Search opened";
       voiceFeedback("Opening search.");
-    },
-    'close search': function() {
-      myWindow.close();
-      lastCommandMessage.innerHTML = "Search window closed";
-      voiceFeedback("Closing search.");
+      url = googleURL;
+      myWindow = window.open(url, "myWindow");
+      //lastCommandMessage.innerHTML = "Search opened.";
+      appendLastCommand("Search opened")
     },
 
-    'thank you': function() {
-      voiceFeedback("You're welcome!");
+    'close search': function() {
+      voiceFeedback("Closing search.");
+      myWindow.close();
+      //lastCommandMessage.innerHTML = "Search window closed.";
+      appendLastCommand("Search window closed.")
     },
+
+    'search for :expression': function(expression) {
+      voiceFeedback("Searching for " + expression);
+      url = googleSearchFor + expression;
+      myWindow = window.open(url, "myWindow");
+      //lastCommandMessage.innerHTML = "Performed search for " + expression;
+    },
+
+    'show me :expression': function(expression) {
+      voiceFeedback("Showing " + expression);
+      googleImageSearchFor = imageSearchStart + expression + imageSearchEnd;
+      myWindow = window.open(googleImageSearchFor, "myWindow");
+      //lastCommandMessage.innerHTML = "Performed image search for " + expression;
+    },
+    /*
+    ':gratitude': function(gratitude) {
+      if (gratitude === "thanks" || gratitude === "thank you") {
+        voiceFeedback("You're welcome!");
+      }
+    },*/
 
     'stop': function() {
       voiceFeedback("I'm stopping.");
     },
 
-    'a, for': function() {
-      voiceFeedback("alpha.", 1, 1);
-    }
+    'test': function() {
+      voiceFeedback("Test, check check!", 1, 2);
+      appendLastCommand("Test.")
+    },
+
+    'shut down': function() {
+      voiceFeedback("I'll close my ears.");
+      annyang.abort();
+    },
+
+    'take a break': function() {
+      voiceFeedback("Okay, let me know if you need me.");
+      annyang.pause();
+    },
+
+    // does not work
+    'online': function() {
+      voiceFeedback("I'm back.");
+      annyang.resume();
+    },
+
+    'phonetic a': function() { voiceFeedback("alpha", 1, 1); },
+    'phonetic b': function() { voiceFeedback("bravo", 1, 1); },
+    'phonetic c': function() { voiceFeedback("charlie", 1, 1); }
   };
 
   // Add commands to annyang
@@ -91,3 +161,29 @@ if (annyang) {
   // Start listening. Call this here, or attach this call to an event, button, etc.
   annyang.start();
 }
+
+
+
+/*
+// test this
+var recognition =  new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
+recognition.continuous = true;
+recognition.interimResults = true;
+recognition.lang = "en-GB";
+
+recognition.onresult = function(event) {
+    var interim_transcript = '';
+
+    for (var i = event.resultIndex; i < event.results.length; ++i) {
+      if (event.results[i].isFinal) {
+        final_transcript += event.results[i][0].transcript;
+      } else {
+        interim_transcript += event.results[i][0].transcript;
+      }
+    }
+
+    console.log(interim_transcript,final_transcript);
+};
+recognition.start();
+
+*/
